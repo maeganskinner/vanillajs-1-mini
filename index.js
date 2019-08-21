22de591423f01da00f282a87dfbfeeb3af5780d6
maeganskinnerblog <maeganskinnerblog@gmail.com>
2019-08-21 15:36:08 -0500

    another try

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var element=document.getElementById("counter");
+
+let count = 0;
+
+function increase(){
+    count++;
+    document.getElementById("counter").innerText=count
+}
+
+function decrease(){
+    count--;
+    document.getElementById("counter").innerText=count
+}
+
+function reset(){
+    count=0
+    document.getElementById("counter").innerHTML="<mark>" + count + "</mark>"
+}
+
+
+ function selectTheme(theme)[
+     document.getElementsByTagName("body").className=theme
+ ]
+ function selectTheme(theme)[
+     document.getElementsByTagName("main").className=theme
+ ]
+function selectTheme(theme)[
+    document.getElementsByTagName("buttons").className=theme
+    for ([];[];[])
+    statement 
+]
+ 
